1ca92071ca37d8181d0e497b88aa16b341f7b706
Ali <soyseyitali23@gmail.com>
2023-03-12 16:39:50 +0300

    Entity Repository altyapısı kuruldu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Concrete;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
@@ -9,11 +10,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            foreach (var car in carManager.GetAll())
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            foreach (var car in carManager.GetCarsByBrandId(1))
             {
-                Console.WriteLine(car.ModelYear);
+                Console.WriteLine(car.Description);
             }
+
+            CarManager carManager1 = new CarManager(new EfCarDal());
+            carManager1.Add(new Entities.Concrete.Car
+            {
+                Id = 5,
+                BrandId = 1,
+                ColorId = 1,
+                DailyPrice = 0,
+                Description="Very Nice Car",
+                ModelYear = 2020              
+            }); ;
         }
     }
 }
